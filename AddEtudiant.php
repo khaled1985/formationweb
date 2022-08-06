9ef9db4b42dbd8a624b34dd88b4a739a7e7e09bb
khaled1985 <elhech.khaled@gmail.com>
2022-08-06 08:33:38 +0100

    liste Etudiants

--- AddEtudiant.php
+++ AddEtudiant.php
@@ -1,7 +1,19 @@
 <?php
+ 
+echo '
+<table  >
+<tr><td>nom:</td><td><input type="text" id="nom"></td> </tr>
+ 
+<tr><td>prenom : </td><td> <input type="text" id="prenom" ></td> </tr>
+<tr><td>Age :</td><td><input type="number" id="age"></td> </tr>
+<tr><td>CIN :</td><td> <input type="number" id="cin"></td> </tr>
+<tr><td colspan=2> <input type="button" value="Enregistrer" class="btn btn-success" onclick="Addetudi()"></td> </tr>
+ 
+</table>
 
-echo "mon premier script php";
-
+' ;
+  
+ 
 
 
 ?>
---
+++ Add_bd_etudiant.php
@@ -0,0 +1,12 @@
+<?php
+
+$nom=$_POST["nom"];
+$prenom=$_POST["prenom"];
+$age=$_POST["age"];
+$cin=$_POST["cin"];
+
+
+ 
+
+
+?>
