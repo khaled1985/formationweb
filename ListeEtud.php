cc0c8ff79b2cc0df8966d23e15da727287d465de
khaled1985 <elhech.khaled@gmail.com>
2024-06-29 13:52:19 +0100

    Revert "feat(github): second test branche"
    This reverts commit 82dbfa903bf2333b714152799901d95315f78c38.

--- ListeEtud.php
+++ ListeEtud.php
@@ -4,7 +4,6 @@ require_once 'db_connect.php';
 $query = "SELECT  * FROM etudiants ";
 //premier test github
  //test branche
- //second test
 $content="";
 if ($result = mysqli_query($connect, $query)) { 
 
